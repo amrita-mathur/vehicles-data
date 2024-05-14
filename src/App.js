65066c54ac642b2d6674d5e2f5ddfdb42dbd55c3
Amrita Mathur <amrita.mathur@greatlearning.in>
2024-05-14 14:38:50 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import VehicleForm from './components/VehicleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleForm/>
+    </div>
+  );
+}
+
+export default App;
